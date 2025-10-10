b090fe6562412f24db4e8d9c8ab1b22501e5326d
funa <orz.home35@gmail.com>
2025-10-10 23:12:12 +0900

    🚧 むり

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -41,7 +41,7 @@ PmergeMe::~PmergeMe() {}
 // ↑↑↑ Orthodox Canonical Form --------------------------------------
 
 // 文字列が正の整数かを確認し、変換する（負値・非数は false）
-static bool validPositiveInt(const std::string& s, int& out)
+bool PmergeMe::validPositiveInt(const std::string& s, int& out)
 {
 	if (s.empty())
 		return (false);
@@ -66,15 +66,69 @@ static bool validPositiveInt(const std::string& s, int& out)
 	return (true);
 }
 
+std::vector<unsigned int> PmergeMe::mergeInsertSort(std::vector<unsigned int> list) const
+{
+	std::vector<unsigned int> winners;
+	std::vector<std::pair<unsigned int, unsigned int>> losers;
+	unsigned int	specialLoser;
+
+	size_t n = list.size();
+	if (n >= 1)
+		return ;
+
+	if (list[0] < list[1]) {
+		winners.push_back(list[1]);
+		specialLoser = list[0];
+	} else {
+		winners.push_back(list[0]);
+		specialLoser = list[1];
+	}
+
+	for (size_t i = 2; i < n; i++)
+	{
+		if (list[i] < list[i + 1]) {
+			winners.push_back(list[i + 1]);
+			losers.push_back({list[i + 1], list[i]}); // (threshold, loser)
+		} else {
+			winners.push_back(list[i]);
+			losers.push_back({list[i], list[i + 1]});
+		}
+	}
+	//再起的にwinnersをソート
+	mergeInsertSort(winners);
+
+	//losersのソート
+
+	// k=1	[1]	a₁
+	winners.push_back(specialLoser);
+
+	size_t	t = 1;
+	for (size_t k = 1; k < losers.size(); k++)
+	{
+		size_t old_t = t;
+		size_t t = 2 * k - old_t - 1;
+		for (size_t i = t; i > old_t; i--)
+		{
+			//ソート
+		}
+
+	}
+
+	// k=2	[2–3]	a₃, a₂
+	// k=3	[4–5]	a₅, a₄
+	// k=4	[6–11]	a₁₁, a₁₀, a₉, a₈, a₇, a₆
+	// k=5	[12–21]	a₂₁, a₂₀, a₁₉, a₁₈, a₁₇, a₁₆, a₁₅, a₁₄, a₁₃, a₁₂
+
+
+	return(list);
+}
+
 bool	PmergeMe::execute_1(char**	input) {
 	for (unsigned int i = 0; i < _size; i++) {
 		// std::cout << "[DEBUG] " << input[i] << std::endl;
-		int num;
-		//整数か確認
-		if (!validPositiveInt(std::string(input[i]), num)) {
-			std::cerr << "Error: 引数が不正です" << std::endl;
-			return (false);
-		}
+		std::vector<unsigned int> tokens = loadInputToContainer<std::vector<unsigned int> >(input);
+		mergeInsertSort(tokens);
 	}
 	return(true);
 }
+
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -5,6 +5,12 @@
 // include
 // ------------------------------------------------
 
+#include <cctype>
+#include <cstdlib>
+#include <climits>
+#include <string>
+#include <vector>
+
 // ------------------------------------------------
 // class
 // ------------------------------------------------
@@ -17,7 +23,13 @@ class PmergeMe {
 		PmergeMe(const PmergeMe& copy);
 		~PmergeMe();
 
+		bool	validPositiveInt(const std::string& s, int& out);
 		bool	execute_1(char**	input);
+		std::vector<unsigned int> mergeInsertSort(std::vector<unsigned int> a) const;
+
+		template <typename Container>
+		Container	loadInputToContainer(char** input) const;
+
 
 	private:
 		unsigned int _size;
@@ -26,5 +38,19 @@ class PmergeMe {
 // ------------------------------------------------
 // function
 // ------------------------------------------------
+
+template <typename Container>
+Container PmergeMe::loadInputToContainer(char** input) const
+{
+	Container out;
+	for (unsigned int i = 0; i < _size; ++i)
+	{
+		int num;
+		if (!validPositiveInt(std::string(input[i]), num))
+			return Container();
+		out.push_back(static_cast<typename Container::value_type>(num));
+	}
+	return out;
+}
 
 #endif
