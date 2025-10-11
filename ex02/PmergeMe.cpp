9bebd7aea10bce14412fd3ba3fb69042699fca28
funa <orz.home35@gmail.com>
2025-10-11 22:03:49 +0900

    🛠️ winnerとloserの位置関係が連動するバージョンを作った

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -93,77 +93,185 @@ static int jacobsthal(int n) {
 	return (curr);
 }
 
+// void PmergeMe::mergeInsertSort(std::vector<unsigned int>& list, bool isTopLevel) const
+// {
+// 	std::vector<unsigned int> chain;
+// 	std::vector<std::pair<unsigned int, unsigned int> > losers;
+
+// 	size_t n = list.size();
+// 	if (n <= 1)
+// 		return ;
+
+// 	if (n % 2 == 1) {
+// 		unsigned int extra = list.back();
+// 		std::cout << "\n[DEBUG extra]: " << extra << "\n";
+
+// 		list.pop_back();
+// 		mergeInsertSort(list, false);
+// 		binaryInsert(list, extra);
+// 		return ;
+// 	}
+
+// 	for (size_t i = 0; i + 1 < n; i += 2)
+// 	{
+// 		unsigned int a = list[i];
+// 		unsigned int b = list[i + 1];
+// 		std::cout << "[DEBUG] i = " << i << "... list[i]: " << a << ", list[i + 1]: " << b << "\n";
+// 		if (a < b) {
+// 			chain.push_back(b);
+// 			losers.push_back(std::make_pair(b, a));//(winner, losers)
+// 		} else {
+// 			chain.push_back(a);
+// 			losers.push_back(std::make_pair(a, b));
+// 		}
+// 	}
+
+// 	//再起的にchainをソート
+// 	mergeInsertSort(chain, false);
+
+
+// 	// //losersのソート
+// 	if (isTopLevel) {
+// 		// chain[0] とペアを組んでいる loser を探す
+// 		unsigned int firstWinner = chain[0];
+// 		for (size_t i = 0; i < losers.size(); ++i) {
+// 			if (losers[i].first == firstWinner) {
+// 				// このペアの loser を先頭に挿入
+// 				unsigned int firstLoser = losers[i].second;
+// 				std::cout << "\n[DEBUG firstLoser]: " << firstLoser;
+// 				chain.insert(chain.begin(), firstLoser);
+
+// 				// この loser は処理済みなので losers から削除
+// 				losers.erase(losers.begin() + i);
+// 				break;
+// 			}
+// 		}
+// 	}
+
+// 	std::cout << "\n[DEBUG chain]:";
+// 	for (size_t i = 0; i < chain.size(); i++) {
+// 		std::cout << " " << chain[i];
+// 	}
+// 	std::cout << "\n";
+
+// 	std::cout << "\n[DEBUG losers]:";
+// 	for (size_t i = 0; i < losers.size(); i++) {
+// 		std::cout << " (threshold: " << losers[i].first << ", loser: " << losers[i].second << ")";
+// 	}
+// 	std::cout << "\n";
+
+// 	// ヤコブスタール数列を使ってlosersを挿入
+// 	int start = 0;
+// 	int k = 1;
+
+// 	while (start < (int)losers.size()) {
+// 		int end = jacobsthal(k);
+// 		if (end > (int)losers.size())
+// 			end = (int)losers.size();
+
+// 		for (int i = end - 1; i >= start; --i) {
+// 			unsigned int winner = losers[i].first;
+// 			unsigned int loser = losers[i].second;
+
+// 			std::cout << "[DEBUG INSERT] " << k << ": end=" << end << ", target=" << i << ", (loser=" << loser << " (winner=" << winner << ")\n";
+
+// 			// winner の位置を探す
+// 			std::vector<unsigned int>::iterator winnerIt = std::lower_bound(chain.begin(), chain.end(), winner);
+
+// 			// [begin, thIt) の範囲で loser を二分挿入
+// 			std::vector<unsigned int>::iterator ins = std::lower_bound(chain.begin(), winnerIt, loser);
+// 			chain.insert(ins, loser);
+
+// 		}
+// 		start = end;
+// 		k++;
+// 	}
+
+// 	list.swap(chain);
+// }
+
 void PmergeMe::mergeInsertSort(std::vector<unsigned int>& list, bool isTopLevel) const
 {
-	std::vector<unsigned int> chain;
-	std::vector<std::pair<unsigned int, unsigned int> > losers;
+	// winner の値と、元のインデックスと loser をセットで管理
+	std::vector<std::pair<unsigned int, size_t> > chainWithIndex;  // (winner値, 元index)
+	std::vector<unsigned int> losersByIndex;  // インデックスで loser を管理
 
 	size_t n = list.size();
 	if (n <= 1)
 		return ;
 
+	// 奇数処理
+	unsigned int extra = 0;
+	bool hasExtra = false;
 	if (n % 2 == 1) {
-		unsigned int extra = list.back();
-		std::cout << "\n[DEBUG extra]: " << extra << "\n";
-
+		extra = list.back();
 		list.pop_back();
-		mergeInsertSort(list, false);
-		binaryInsert(list, extra);
-		return ;
+		hasExtra = true;
+		n--;
 	}
 
-	for (size_t i = 0; i + 1 < n; i += 2)
-	{
+	// 全ペアを処理
+	for (size_t i = 0; i + 1 < n; i += 2) {
 		unsigned int a = list[i];
 		unsigned int b = list[i + 1];
-		std::cout << "[DEBUG] i = " << i << "... list[i]: " << a << ", list[i + 1]: " << b << "\n";
+		size_t pairIndex = i / 2;  // ペアのインデックス
+
 		if (a < b) {
-			chain.push_back(b);
-			losers.push_back(std::make_pair(b, a));//(winner, losers)
+			chainWithIndex.push_back(std::make_pair(b, pairIndex));
+			losersByIndex.push_back(a);
 		} else {
-			chain.push_back(a);
-			losers.push_back(std::make_pair(a, b));
+			chainWithIndex.push_back(std::make_pair(a, pairIndex));
+			losersByIndex.push_back(b);
 		}
 	}
 
-	//再起的にchainをソート
+	// winner の値だけを取り出して再帰ソート
+	std::vector<unsigned int> chain;
+	for (size_t i = 0; i < chainWithIndex.size(); ++i) {
+		chain.push_back(chainWithIndex[i].first);
+	}
+
 	mergeInsertSort(chain, false);
 
+	// 再帰後：chain の順序に合わせて losers を再構築
+	std::vector<std::pair<unsigned int, unsigned int> > losers;
+	for (size_t i = 0; i < chain.size(); ++i) {
+		unsigned int winner = chain[i];
 
-	// //losersのソート
-	if (isTopLevel) {
-		// chain[0] とペアを組んでいる loser を探す
-		unsigned int firstWinner = chain[0];
-		for (size_t i = 0; i < losers.size(); ++i) {
-			if (losers[i].first == firstWinner) {
-				// このペアの loser を先頭に挿入
-				unsigned int firstLoser = losers[i].second;
-				std::cout << "\n[DEBUG firstLoser]: " << firstLoser;
-				chain.insert(chain.begin(), firstLoser);
+		// この winner の元のインデックスを探す
+		for (size_t j = 0; j < chainWithIndex.size(); ++j) {
+			if (chainWithIndex[j].first == winner) {
+				size_t originalIndex = chainWithIndex[j].second;
+				unsigned int loser = losersByIndex[originalIndex];
+				losers.push_back(std::make_pair(winner, loser));
 
-				// この loser は処理済みなので losers から削除
-				losers.erase(losers.begin() + i);
-				break;
+				// 同じ値が複数ある場合の対策（使用済みをマーク）
+				chainWithIndex[j].first = UINT_MAX;
+				break ;
 			}
 		}
 	}
 
-	std::cout << "\n[DEBUG chain]:";
-	for (size_t i = 0; i < chain.size(); i++) {
-		std::cout << " " << chain[i];
+	// isTopLevel なら chain[0] のペアを先頭に挿入
+	if (isTopLevel && !losers.empty()) {
+		unsigned int firstLoser = losers[0].second;
+		chain.insert(chain.begin(), firstLoser);
+		losers.erase(losers.begin());
 	}
+
+	std::cout << "\n[DEBUG chain]:";
+	for (size_t i = 0; i < chain.size(); i++)
+		std::cout << " " << chain[i];
 	std::cout << "\n";
 
 	std::cout << "\n[DEBUG losers]:";
-	for (size_t i = 0; i < losers.size(); i++) {
+	for (size_t i = 0; i < losers.size(); i++)
 		std::cout << " (threshold: " << losers[i].first << ", loser: " << losers[i].second << ")";
-	}
 	std::cout << "\n";
 
 	// ヤコブスタール数列を使ってlosersを挿入
 	int start = 0;
 	int k = 1;
-
 	while (start < (int)losers.size()) {
 		int end = jacobsthal(k);
 		if (end > (int)losers.size())
@@ -186,6 +294,9 @@ void PmergeMe::mergeInsertSort(std::vector<unsigned int>& list, bool isTopLevel)
 		start = end;
 		k++;
 	}
+
+	if (hasExtra)
+		binaryInsert(chain, extra);
 
 	list.swap(chain);
 }
